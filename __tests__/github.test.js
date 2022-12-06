2763c2148fab3468a5d60348529f944d87b08721
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-12-05 19:21:25 -0600

    passing test two

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -21,7 +21,18 @@ describe('github auth', () => {
     );
   });
 
-  it.skip('/api/v1/github/callback should login users and redirect to dashboard', async () => {
-    const res = await request;
+  it('/api/v1/github/callback should login users and redirect to dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      login: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: 'https://www.placecage.com/gif/300/300',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 });
