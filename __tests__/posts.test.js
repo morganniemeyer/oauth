6abdd7fd5912893818e0483034020db788b2f061
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-12-06 13:53:35 -0600

    completed get all posts test

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -2,6 +2,7 @@ const setup = require('../data/setup.js');
 const pool = require('../lib/utils/pool.js');
 const request = require('supertest');
 const app = require('../lib/app');
+const Post = require('../lib/models/post.js');
 
 jest.mock('../lib/services/github');
 
@@ -26,5 +27,18 @@ describe('posts', () => {
       post: newPost.post,
       user_id: newPost.user_id,
     });
+  });
+  it('GET /api/v1/posts should return a list of all posts if a user is logged in', async () => {
+    const agent = request.agent(app);
+    const user = await agent
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    const newPost = await Post.insert({
+      post: 'another test time!!',
+      user_id: user.body.id,
+    });
+    const res = await agent.get('/api/v1/posts');
+    expect(res.status).toBe(200);
+    expect(res.body[0]).toEqual(newPost);
   });
 });
